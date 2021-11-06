1b69a0af2716fe78e0ce009611a3a83e715c747b
Simeon David Schaub <schaub@mit.edu>
2021-11-06 18:55:43 -0400

    use spaces for indentation

--- src/SchurPolynomials.jl
+++ src/SchurPolynomials.jl
@@ -1,51 +1,58 @@
 module SchurPolynomials
 
-using AbstractAlgebra, FGenerators, Transducers, DynamicPolynomials
+using AbstractAlgebra, Transducers, DynamicPolynomials
+using AbstractAlgebra: Partition
+using Transducers: next, @next, complete
 import MutableArithmetics as MA
 
 struct SemiStandardYoungTableaux <: Foldable
-	T::Generic.YoungTableau{Int}
-	rows::Vector{SubArray{Int64, 1, Vector{Int}, Tuple{UnitRange{Int}}, true}}
+    T::Generic.YoungTableau{Int}
+    rows::Vector{SubArray{Int64, 1, Vector{Int}, Tuple{UnitRange{Int}}, true}}
     n::Int
-	function SemiStandardYoungTableaux(λ, n)
-		fill = Vector{Int}(undef, sum(λ)) #[i for (i, j) in enumerate(λ) for _ in 1:j]
-		rows = map(λ, Iterators.accumulate(+, λ)) do len, stop
-			view(fill, stop-len+1:stop)
-		end
-		return new(YoungTableau(λ, fill), rows, n)
-	end
+    function SemiStandardYoungTableaux(λ, n)
+        fill = Vector{Int}(undef, sum(λ)) #[i for (i, j) in enumerate(λ) for _ in 1:j]
+        rows = map(λ, Iterators.accumulate(+, λ)) do len, stop
+            view(fill, stop-len+1:stop)
+        end
+        return new(YoungTableau(λ, fill), rows, n)
+    end
 end
 
-@fgenerator function row_configs!(rows, i, n)
-	row = @inbounds rows[i]
-	# row i, column j
-	function _row!(j)
-		j > length(row) && (@yield(row); return)
-		above = i == 1 ? 0 : rows[i-1][j]
-		left = get(row, j-1, 1)
-		@inbounds for row[j] in max(above + 1, left):n
-			_row!(j + 1)
-		end
-	end
-	_row!(1)
+function row_configs!(rows, i, n)
+    Transducers.AdHocFoldable((; rows, i, n)) do rf, acc, (; rows, i, n)
+        row = @inbounds rows[i]
+        # row i, column j
+        function _row!(j, acc)
+            j > length(row) && return next(rf, acc, row)
+            above = i == 1 ? 0 : rows[i-1][j]
+            left = get(row, j-1, 1)
+            @inbounds for row[j] in max(above + 1, left):n
+                acc = _row!(j + 1, acc)
+                acc isa Reduced && return acc
+            end
+            acc
+        end
+        acc = _row!(1, acc)
+        return complete(rf, acc)
+    end
 end
 
-@fgenerator(ssyt::SemiStandardYoungTableaux) do
-    (; T, rows, n) = ssyt
-	function worker!(i)
-		i > length(rows) && (@yield(T); return)
-		foreach(row_configs!(rows, i, n)) do _
-			worker!(i + 1)
-		end
-	end
-	worker!(1)
+function Transducers.__foldl__(rf, acc, (; T, rows, n)::SemiStandardYoungTableaux)
+    function worker!(i, acc)
+        i > length(rows) && return next(rf, acc, T)
+        foldl(row_configs!(rows, i, n); init=acc) do acc, _
+            worker!(i + 1, acc)
+        end
+    end
+    acc = worker!(1, acc)
+    return complete(rf, acc)
 end
 
 function Base.length((; T, n)::SemiStandardYoungTableaux)
     return Int(prod(
-    	(n-i+j) // hooklength(T, i, j)
-    	for (i, j_max) in enumerate(T.part)
-    	for j in 1:j_max
+        (n-i+j) // hooklength(T, i, j)
+        for (i, j_max) in enumerate(T.part)
+        for j in 1:j_max
     ))
 end
 Base.eltype(::SemiStandardYoungTableaux) = Generic.YoungTableau
@@ -55,11 +62,11 @@ function schur(n, λ)
     k = length(SSYT)
     @polyvar x[1:n]
     monomials = [Vector{Int}(undef, n) for _ in 1:k]
-	foreach(Enumerate(), SSYT) do (j, T)
+    foreach(Enumerate(), SSYT) do (j, T)
         map!(monomials[j], 1:n) do i
-		    count(==(i), T.fill)
+            count(==(i), T.fill)
         end
-	end
+    end
     return DynamicPolynomials.polynomialclean(x, fill(1, k), monomials)
 end
 

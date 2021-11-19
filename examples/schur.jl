120f8e45376021cceaabb574da193c01b89c0145
Simeon David Schaub <schaub@mit.edu>
2021-11-19 16:35:41 -0500

    wip

--- examples/schur.jl
+++ examples/schur.jl
@@ -8,26 +8,20 @@ using InteractiveUtils
 begin
 	import Pkg
 	Pkg.activate(".")
-	Pkg.add(; url="git@github.com:simeonschaub/YAPP.jl", rev="main")
+	#Pkg.add(; url="git@github.com:simeonschaub/YAPP.jl", rev="main")
+	Pkg.develop(; path="/home/simeon/.julia/dev/YAPP")
 	Pkg.develop(; path="..")
-	Pkg.add(["AbstractAlgebra", "Transducers", "Dictionaries"]) 
-	Pkg.add(["BenchmarkTools", "PlutoUI"]) 
+	Pkg.add(["AbstractAlgebra", "Transducers", "Dictionaries", "BenchmarkTools", "PlutoUI"]) 
 end
 
 # ╔═╡ ee3c9bca-3fa0-11ec-3fc4-55bf8b43afb4
-using SchurPolynomials, AbstractAlgebra, YAPP, Transducers, Dictionaries, LinearAlgebra
-
-# ╔═╡ cace95fd-5ec0-4ccc-85f1-0a5227327660
-using YAPP: Polynomial, Monomial
-
-# ╔═╡ 08db170f-1389-40ca-a03a-bd4dae06d1bf
-using SchurPolynomials: schur
-
-# ╔═╡ 3049ac04-180c-45a1-9571-58afdb69dbbc
-using Transducers: Map
-
-# ╔═╡ 4a3fb69b-13c7-4fdf-b12f-3bd1e2abdaba
-using AbstractAlgebra: Partition
+begin
+	using SchurPolynomials, AbstractAlgebra, YAPP, Transducers, Dictionaries, LinearAlgebra
+	using YAPP: Polynomial, Monomial
+	using SchurPolynomials: schur
+	using Transducers: Map
+	using AbstractAlgebra: Partition
+end
 
 # ╔═╡ 9e830e0c-2cab-4247-8487-ece4b6f82854
 using BenchmarkTools
@@ -80,19 +74,23 @@ let x = rand(n)
 	(Ref(x) .|> Dictionary(S)) .- (Ref(x) .|> Dictionary(S′))
 end
 
-# ╔═╡ a3eb1251-3e72-422e-a53c-0faca447964f
-@time 1+1
-
 # ╔═╡ b3f710d2-5216-47a8-8427-3b0a5d0f2dd7
 @benchmark schur(10, Partition([4, 3, 2, 1]))
+
+# ╔═╡ 9ee2519b-786a-428e-a517-7e599c11f3e4
+let m=100, n=5
+	x_ = randn(n, m)
+	p, s = (Matrix{Float64}(undef, length(Generic.partitions(n)), m) for _ in 1:2)
+	for (i, λ) in enumerate(Generic.partitions(n))
+		p[i, :] .= (eachcol(x_) .|> powerfun(λ, x[1:n])) ./ z(λ)
+		s[i, :] .= (eachcol(x_) .|> schur(n, λ))
+	end
+	round.(Int, s / p)
+end
 
 # ╔═╡ Cell order:
 # ╠═76f9e6bd-6c96-4441-8f80-dec874ea5a11
 # ╠═ee3c9bca-3fa0-11ec-3fc4-55bf8b43afb4
-# ╠═cace95fd-5ec0-4ccc-85f1-0a5227327660
-# ╠═08db170f-1389-40ca-a03a-bd4dae06d1bf
-# ╠═3049ac04-180c-45a1-9571-58afdb69dbbc
-# ╠═4a3fb69b-13c7-4fdf-b12f-3bd1e2abdaba
 # ╠═480c3e86-ef27-437d-872d-612792f259f9
 # ╠═fd2f0cdb-0674-4546-bae8-15dcacc5a18c
 # ╠═53eee919-2c7b-4c0f-bd86-9e715d2b0129
@@ -105,6 +103,6 @@ end
 # ╠═d4b75503-c43a-4c58-880b-628f5e9e0972
 # ╠═ceebd22f-447c-4310-8a52-b22ec98ce876
 # ╠═8a08ba51-f000-4771-854a-301133c7c405
-# ╠═a3eb1251-3e72-422e-a53c-0faca447964f
 # ╠═9e830e0c-2cab-4247-8487-ece4b6f82854
 # ╠═b3f710d2-5216-47a8-8427-3b0a5d0f2dd7
+# ╠═9ee2519b-786a-428e-a517-7e599c11f3e4
